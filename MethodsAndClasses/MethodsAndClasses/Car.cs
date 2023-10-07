75525cd86a24e32be0744ca18c7a6236eaf23383
Elvin <elvinsrkrov@gmail.com>
2023-10-07 21:49:05 +0400

    tasks added

---
+++ MethodsAndClasses/MethodsAndClasses/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndClasses
+{
+    public class Car
+    {
+        public string Brand;
+        public string Model;
+        public double CurrentFuel;
+        public double FuelFor1Km;
+        public double Millage;
+
+        public Car(string brand, string model, double currentFuel, double fuelFor1Km, double millage)
+        {
+            this.Brand = brand;
+            this.Model = model;
+            this.CurrentFuel = currentFuel;
+            this.FuelFor1Km = fuelFor1Km;
+            this.Millage = millage;
+        }
+
+        public void Drive(int distance)
+        {
+            double requiredFuel = distance * FuelFor1Km / 1000;
+            if (requiredFuel <= CurrentFuel)
+            {
+                Millage += distance;
+                CurrentFuel -= requiredFuel;
+                Console.WriteLine($"Sürüş başa çatdı. Millage: {Millage} km, Benzin miqdarı: {CurrentFuel} l.");
+            }
+            else
+            {
+                Console.WriteLine("Sürüş üçün Benzin yeterli deyil!!");
+            }
+        }
+    }
+}
---
+++ MethodsAndClasses/MethodsAndClasses/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace MethodsAndClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetValueByConsole();
+
+
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            AddElementToArr(ref numbers, 12);
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Car car = new Car("BMW", "X6", 40, 3, 20000);
+            car.Drive(12000);
+
+            #region ThreeMethod
+            static void GetValueByConsole()
+            {
+                Console.Write("Enter the first input: ");
+                string word1 = Console.ReadLine();
+                Console.Write("Enter the second input: ");
+                string word2 = Console.ReadLine();
+                IntOrNot(word1, word2);
+            }
+            static bool IntOrNot(string consoleVal1, string consoleVal2)
+            {
+                bool val1 = int.TryParse(consoleVal1, out int result1);
+                bool val2 = int.TryParse(consoleVal2, out int result2);
+
+                Console.WriteLine("Sum of numbers: " + GetSumOfNums(result1, result2));
+
+                if (val1 && val2) return true;
+
+                return false;
+            }
+            static int GetSumOfNums(int num1, int num2)
+            {
+                return num1 + num2;
+            }
+            #endregion
+
+            #region Add New Element to the Array
+            static void AddElementToArr(ref int[] array, int newArrItem)
+            {
+                int[] newArr = new int[array.Length + 1];
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    newArr[i] = array[i];
+                }
+                newArr[^1] = newArrItem;
+                array = newArr;
+            }
+            #endregion
+
+        }
+    }
+}
